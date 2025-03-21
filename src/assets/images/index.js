30ee6a79252ef82afaae3b604145b22e5e739eea
Asish K Beniya <asishbeniya@gmail.com>
2025-03-22 01:19:25 +0530

    logo changed

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,12 +1,10 @@
-import hero from './hero.jpg'
-import meta from './meta.png'
-import shopify from './shopify.png'
+import meta from './kiit.png'
+import shopify from './bjb.jpeg'
 import starbucks from './starbucks.png'
-import tesla from './tesla.png'
+import tesla from './xavier.jpeg'
 import logo from './logo.svg'
 
 export {
-    hero,
     meta,
     shopify,
     starbucks,
